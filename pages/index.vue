223b0e510426b56edcc675e9f08dd7a7e711d786
Mick <mickheudre@gmail.com>
2022-10-25 17:41:27 +0200

    sort by date

--- pages/index.vue
+++ pages/index.vue
@@ -18,7 +18,17 @@
     
     components: { Agenda },
     async asyncData({ $axios }) {
-      const events = await $axios.$post("https://api.notion.com/v1/databases/f50d073e296d4013b0d91b731e3d7d25/query", {});
+
+      const data = {
+            "sorts": [
+            {
+                "property": "Date",
+                "direction": "descending"
+            }
+            ]
+        }
+
+      const events = await $axios.$post("https://api.notion.com/v1/databases/f50d073e296d4013b0d91b731e3d7d25/query", data );
       return { events };
     },
   })
