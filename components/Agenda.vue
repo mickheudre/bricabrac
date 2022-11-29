8e6332b3ec0374292c1a17735362505d24af324a
mick <mickheudre@gmail.com>
2022-11-29 10:27:00 +0100

    mise en forme dates

--- components/Agenda.vue
+++ components/Agenda.vue
@@ -11,9 +11,9 @@
             <tbody class="">
                 <tr v-for="event in events" :key="event.id">
                 <template v-if="!event.properties.Fini.formula.boolean">
-                    <td  class="px-4 md:px-6 py-2 underline"><a target="_blank" :href="event.properties.URL.url"> {{ event.properties.Name.title[0].plain_text }}</a></td>
-                    <td  class="px-4 md:px-6 py-2">{{ event.properties.Ville.rich_text[0].plain_text }}</td>
-                    <td  class="px-4 md:px-6 py-2 capitalize"> {{ dateToString(event.properties.Date.date) }} </td>
+                    <td  class="px-4 md:px-6 py-2 underline h-16"><a target="_blank" :href="event.properties.URL.url"> {{ event.properties.Name.title[0].plain_text }}</a></td>
+                    <td  class="px-4 md:px-6 py-2 h-16">{{ event.properties.Ville.rich_text[0].plain_text }}</td>
+                    <td  class="px-4 md:px-6 py-2 capitalize h-16"> {{ dateToString(event.properties.Date.date) }} </td>
                 </template>
                 </tr>
             </tbody>
@@ -30,11 +30,11 @@
                 </tr>                   
             </thead>
             <tbody class="">
-                <tr v-for="event in events" :key="event.id">
+                <tr  v-for="event in events" :key="event.id">
                 <template v-if="event.properties.Fini.formula.boolean">
-                    <td  class="px-4 md:px-6 py-2 underline"><a target="_blank" :href="event.properties.URL.url"> {{ event.properties.Name.title[0].plain_text }}</a></td>
-                    <td  class=" w-1/4 px-4 md:px-6 py-2">{{ event.properties.Ville.rich_text[0].plain_text }}</td>
-                    <td  class="w-1/3 px-4 md:px-6 py-2 capitalize"> {{ dateToString(event.properties.Date.date) }}</tD>
+                    <td  class="px-4 md:px-6 py-2 underline h-16"><a target="_blank" :href="event.properties.URL.url"> {{ event.properties.Name.title[0].plain_text }}</a></td>
+                    <td  class=" w-1/4 px-4 md:px-6 py-2 h-16">{{ event.properties.Ville.rich_text[0].plain_text }}</td>
+                    <td  class="w-1/3 px-4 md:px-6 py-2 capitalize h-16"> {{ dateToString(event.properties.Date.date) }}</tD>
                 </template>
                 </tr>
             </tbody>
@@ -58,16 +58,16 @@
                     if (eventStart.getMonth() === eventEnd.getMonth()) {
 
                         if (eventStart.getDay() === eventEnd.getDay()) {
-                            return eventStart.toLocaleDateString('fr-FR', { weekday: "long", year: "numeric", month: "long", day: "numeric" })
+                            return eventStart.toLocaleDateString('fr-FR', { weekday: "short", year: "numeric", month: "long", day: "numeric" })
                         }
 
-                        return eventStart.toLocaleDateString('fr-FR', { weekday: "long", day: "numeric" }) + " → " + eventEnd.toLocaleDateString('fr-FR', { weekday: "long", year: "numeric", month: "long", day: "numeric" }) 
+                        return eventStart.toLocaleDateString('fr-FR', { weekday: "short", day: "numeric" }) + " → " + eventEnd.toLocaleDateString('fr-FR', { weekday: "short", year: "numeric", month: "long", day: "numeric" }) 
  
                     }
-                    return eventStart.toLocaleDateString('fr-FR', { weekday: "long", year: "numeric", month: "long", day: "numeric" }) + "→" + eventEnd.toLocaleDateString('fr-FR', { weekday: "long", year: "numeric", month: "long", day: "numeric" }) 
+                    return eventStart.toLocaleDateString('fr-FR', { weekday: "short", year: "numeric", month: "long", day: "numeric" }) + "→" + eventEnd.toLocaleDateString('fr-FR', { weekday: "short", year: "numeric", month: "long", day: "numeric" }) 
 
                 }
-                return eventStart.toLocaleDateString('fr-FR', { weekday: "long", year: "numeric", month: "long", day: "numeric" })
+                return eventStart.toLocaleDateString('fr-FR', { weekday: "short", year: "numeric", month: "long", day: "numeric" })
             }
         }
         
