3ba60150c3ebe7ca853ebf4b2b447b1bccae098c
mick <mickheudre@gmail.com>
2022-11-29 10:39:31 +0100

    titre du site

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
   
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'bricabrac',
+    title: 'Bric à brac',
     htmlAttrs: {
       lang: 'en'
     },
