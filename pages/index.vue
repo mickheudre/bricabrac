c075783ae1ee2ca3ea48e9e92f025d3a9f53694f
mick <mickheudre@gmail.com>
2023-01-09 11:56:52 +0100

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="">         
-    <div class="w-full p-4 md:p-8 lg:p-16 flex flex-col">
-      <h1 class="font-brand text-uppercase font-black text-persian text-3xl md:my-2">Bric A Brac</h1>
-      <Page :page="presentation" class="text-persian lg:w-1/2 xl:w-1/3" />
-    </div>
-    <div class="flex flex-col  items-center bg-beige mt-4 2xl:mt-16 ">
-      <Agenda class="w-full lg:w-2/3 2xl:w-1/2 px-2 md:px-8 pb-32" :events="events.results"/>
-    </div>
+  <div class="">    
+    <client-only> 
+      <div class="w-full p-4 md:p-8 lg:p-16 flex flex-col">
+        <h1 class="font-brand text-uppercase font-black text-persian text-3xl md:my-2">Bric A Brac</h1>
+        <Page :page="presentation" class="text-persian lg:w-1/2 xl:w-1/3" />
+      </div>
+      <div class="flex flex-col  items-center bg-beige mt-4 2xl:mt-16 ">
+        <Agenda class="w-full lg:w-2/3 2xl:w-1/2 px-2 md:px-8 pb-32" :events="events.results"/>
+      </div>
+    </client-only>    
   </div>
 </template>
 
--- pages/success.vue
+++ pages/success.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        <p>Votre demande à bien été enregistrée !</p>
-        <nuxt-link to="/">Retour au site</nuxt-link>
+    <div class="min-h-screen text-persian w-screen">
+        <div class="flex flex-col justify-center content-center mx-auto h-screen max-w-md">
+            <h3 class="text-2xl my-4 font-brand">Votre demande à bien été enregistrée</h3>
+            <p>Votre évenement sera ajouté à l'agenda rapidement. Il peut y avoir un délai de quelques jours. N'hésitez pas à nous contacter en cas de problème.</p>
+            <NuxtLink class="underline my-4" to="/">Retour au site</NuxtLink >
+        </div>
     </div>
 </template>
