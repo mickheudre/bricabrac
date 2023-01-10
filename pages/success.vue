68048e22e800a089eb54794b888c2eeec308a9ee
mick <mickheudre@gmail.com>
2023-01-10 10:26:39 +0100

    mini layout fix

--- pages/success.vue
+++ pages/success.vue
@@ -1,9 +1,12 @@
 <template>
-    <div class="min-h-screen text-persian w-screen">
-        <div class="flex flex-col justify-center content-center mx-auto h-screen max-w-md">
+    <div class="h-full text-persian max-w-screen">`
+
+        <div class="mx-auto my-auto h-full max-w-md">
+        <div class="flex flex-col justify-center content-center items-center px-4 h-screen">
             <h3 class="text-2xl my-4 font-brand">Votre demande a bien été enregistrée</h3>
             <p>Votre événement sera ajouté à l'agenda rapidement. Il peut y avoir un délai de quelques jours. N'hésitez pas à nous contacter en cas de problème.</p>
             <NuxtLink class="underline my-4" to="/">Retour au site</NuxtLink >
+        </div>
         </div>
     </div>
 </template>
