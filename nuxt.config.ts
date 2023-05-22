ebb9809c7b7b964c38fd502ecf6dc48ac7f3420b
mick <mickheudre@gmail.com>
2023-05-22 17:18:57 +0200

    seo stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Bric à brac',
+      meta: [
+        { name: 'description', content: "Bric à Brac est un calendrier en ligne des salons d'édition." }
+      ],
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }},
     css: ['~/assets/css/main.css'],
     nitro: {
-        compressPublicAssets: true,
-        prerender: {
-            crawlLinks: true
-          }
-      },
-      postcss: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
+      compressPublicAssets: true,
+      prerender: {
+        crawlLinks: true
       }
-})
+    },
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    }
+  })
+  
