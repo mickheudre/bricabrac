85c67ac2dd1cb02c931535d8b89cfdb6c8d09b39
mick <mickheudre@gmail.com>
2023-05-29 18:25:29 +0200

    Typo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: 'Bric à Brac, le calendrier en ligne des salons de micro édition',
+      title: 'Bric à Brac, le calendrier en ligne des salons de micro-édition',
       meta: [
         { name: 'description', content: "Bric à Brac est un calendrier en ligne des salons d'édition. Vous pouvez y référencer vos salons de micro-édition, d'éditions indépendantes..." }
       ],
