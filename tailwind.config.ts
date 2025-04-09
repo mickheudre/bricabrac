80d0dbeeb27a7066efa934c777f5516bb82cce1a
mick <mickheudre@gmail.com>
2025-04-09 14:46:02 +0200

    Updated Nuxt version

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    content: [
+      "./components/**/*.{js,vue,ts}",
+      "./layouts/**/*.vue",
+      "./pages/**/*.vue",
+      "./plugins/**/*.{js,ts}",
+      "./nuxt.config.{js,ts}",
+      "./app.vue",
+    ],
+    theme: {
+      fontFamily: {
+         brand: ['Movement Direct']
+      },
+      colors: {
+        beige: '#FFF1D2',
+        persian: '#D98061'
+      }
+    },
+    plugins: [],
+  
+  }
+  
+  
